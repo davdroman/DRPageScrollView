eff1724eb363859e66fad06d953d7bef999385a9
davdroman <d@vidroman.dev>
2018-07-14 16:09:03 +0100

    Rename and reset project

---
+++ PageScrollView/PageScrollView.swift
@@ -0,0 +1,11 @@
+//
+//  PageScrollView.swift
+//  PageScrollView
+//
+//  Created by David Roman on 14/07/2018.
+//  Copyright © 2018 David Roman. All rights reserved.
+//
+
+class PageScrollView {
+
+}
