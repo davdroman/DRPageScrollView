bdb80baa8c82611d1e017fa45399997fb40d0f98
davdroman <d@vidroman.dev>
2018-07-14 16:13:54 +0100

    Add demo app target

---
+++ PageScrollViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PageScrollViewDemo
+//
+//  Created by David Roman on 14/07/2018.
+//  Copyright © 2018 David Roman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
