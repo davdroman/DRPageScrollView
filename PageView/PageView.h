29bd7e7490dfe1798f9b7c468096555f855350e2
davdroman <d@vidroman.dev>
2018-09-26 16:59:54 +0100

    Rename to `PageView`

---
+++ PageView/PageView.h
@@ -0,0 +1,18 @@
+//
+//  PageView.h
+//  PageView
+//
+//  Created by David Roman on 26/09/2018.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PageView.
+FOUNDATION_EXPORT double PageViewVersionNumber;
+
+//! Project version string for PageView.
+FOUNDATION_EXPORT const unsigned char PageViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PageView/PublicHeader.h>
+
+
