c85f7c7b6a3ea4f19b088bf3f3d2599dae1a8451
davdroman <d@vidroman.dev>
2018-10-10 12:32:25 +0200

    Implement `String` conformance to `PageViewDataSource`

--- PageView/PageView.swift
+++ PageView/PageView.swift
@@ -23,8 +23,21 @@ extension Array: PageViewDataSource where Element: UIView {
 	}
 }
 
-extension UINib: PageViewDataSource {
-	// TODO
+extension String: PageViewDataSource {
+	private var nibViews: [UIView] {
+		guard let views = UINib(nibName: self, bundle: nil).instantiate(withOwner: nil, options: nil) as? [UIView] else {
+			fatalError("The nib name specified as data source does not contain any views")
+		}
+		return views
+	}
+
+	public func pageView(_ pageView: PageView, viewForPageAt index: Int) -> UIView {
+		return nibViews.pageView(pageView, viewForPageAt: index)
+	}
+
+	public func numberOfPages(in pageView: PageView) -> Int {
+		return nibViews.numberOfPages(in: pageView)
+	}
 }
 
 public final class PageView: UIScrollView {
